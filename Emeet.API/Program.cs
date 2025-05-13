f03294b936ab0639d9b96d8a52a2ca25056c32ef
GaHongHacDev <tientranxuan263@gmail.com>
2025-05-14 00:00:47 +0700

    commit set up project

--- Emeet.API/Program.cs
+++ Emeet.API/Program.cs
@@ -92,6 +92,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 builder.Services.AddAuthorization();
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
